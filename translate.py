cd3907178023080824700a55295ed5cb066c70ee
- <who@s-MacBook-Pro.local>
2017-12-15 12:01:52 -0800

    added test tranalation to main

--- translate.py
+++ translate.py
@@ -83,7 +83,59 @@ def translate(model, src, src_lang, lengths, beam_size, max_trg_len, target_voca
             print idxs_to_string(beam.get_hyp(0), idx2word)
     return idxs_to_string(beam.get_hyp(0), idx2word)
 
-# Example translation
+# # Example translation
+# if __name__ == '__main__':
+#     # Load config
+# 	config_file = 'config_en_fr.json'
+# 	config = load_config(config_file)
+
+# 	# Load saved model 
+# 	print "Loading model"
+# 	model = torch.load('ckpt-bt/model.pt').cpu()
+# 	model.dropout = 0
+# 	model.encoder.rnn.dropout = 0
+# 	model.l1_decoder.dropout = torch.nn.Dropout(0)
+# 	model.l1_decoder.rnn.dropout = torch.nn.Dropout(0)
+# 	model.l2_decoder.dropout = torch.nn.Dropout(0)
+# 	model.l2_decoder.rnn.dropout = torch.nn.Dropout(0)
+
+# 	# Load embeddings and (test) datasets
+# 	l1_embeddings, l1_vocab = load_embeddings(path=config.data.l1_embeddings)
+# 	l2_embeddings, l2_vocab = load_embeddings(path=config.data.l2_embeddings)
+# 	l1_dataset = MonolingualDataset(folder=config.data.l1_test_data,
+# 	                                train=False, 
+# 	                                vocab=l1_vocab)
+# 	l2_dataset = MonolingualDataset(folder=config.data.l2_test_data,
+# 	                                train=False,
+# 	                                vocab=l2_vocab)
+
+# 	# Translate 10 sentences from test set from l2->l1
+# 	# Also time results
+# 	start = time.time()
+# 	beam_size = 12
+# 	for i in range(10): 
+# 		# Hacks needed to get test batches of size 1
+# 		l2_sample = l2_dataset[i]
+# 		l2_src = Variable(torch.LongTensor(l2_sample['src'])).unsqueeze(0)
+# 		l2_lengths = torch.LongTensor([l2_sample['src_len']])
+# 		# End hack
+# 		l2_src, l2_lengths, _, l2_index = transform_inputs(
+# 			src=l2_src,
+# 			lengths=l2_lengths,
+# 			tgt=l2_src)
+
+# 		print translate(model=model, 
+# 	    	src=l2_src, 
+# 	    	src_lang='l2', 
+# 	    	lengths=l2_lengths, 
+# 	    	beam_size=beam_size,
+# 	    	max_trg_len=config.data.max_length, 
+# 	    	target_vocab=l1_vocab) # MAKE SURE target_vocab IS TARGET, NOT SOURCE VOCAB
+# 	print time.time() - start
+
+
+
+# Translate test set
 if __name__ == '__main__':
     # Load config
 	config_file = 'config_en_fr.json'
@@ -92,45 +144,58 @@ if __name__ == '__main__':
 	# Load saved model 
 	print "Loading model"
 	model = torch.load('ckpt-bt/model.pt').cpu()
-	model.dropout = 0
-	model.encoder.rnn.dropout = 0
-	model.l1_decoder.dropout = torch.nn.Dropout(0)
-	model.l1_decoder.rnn.dropout = torch.nn.Dropout(0)
-	model.l2_decoder.dropout = torch.nn.Dropout(0)
-	model.l2_decoder.rnn.dropout = torch.nn.Dropout(0)
+	model.eval()
 
 	# Load embeddings and (test) datasets
 	l1_embeddings, l1_vocab = load_embeddings(path=config.data.l1_embeddings)
 	l2_embeddings, l2_vocab = load_embeddings(path=config.data.l2_embeddings)
-	l1_dataset = MonolingualDataset(folder=config.data.l1_test_data,
-	                                train=False, 
-	                                vocab=l1_vocab)
-	l2_dataset = MonolingualDataset(folder=config.data.l2_test_data,
-	                                train=False,
-	                                vocab=l2_vocab)
 
-	# Translate 10 sentences from test set from l2->l1
-	# Also time results
+	# Translate all test files
 	start = time.time()
 	beam_size = 12
-	for i in range(10): 
-		# Hacks needed to get test batches of size 1
-		l2_sample = l2_dataset[i]
-		l2_src = Variable(torch.LongTensor(l2_sample['src'])).unsqueeze(0)
-		l2_lengths = torch.LongTensor([l2_sample['src_len']])
-		# End hack
-		l2_src, l2_lengths, _, l2_index = transform_inputs(
-			src=l2_src,
-			lengths=l2_lengths,
-			tgt=l2_src)
 
-		print translate(model=model, 
-	    	src=l2_src, 
-	    	src_lang='l2', 
-	    	lengths=l2_lengths, 
-	    	beam_size=beam_size,
-	    	max_trg_len=config.data.max_length, 
-	    	target_vocab=l1_vocab) # MAKE SURE target_vocab IS TARGET, NOT SOURCE VOCAB
-	print time.time() - start
+	test_dirs = ['ref_en', 'ref_fr', 'src_en', 'src_fr']
+	test_dirs = ['test_data/' + f for f in test_dirs]
+
+	for test_dir in test_dirs:
+	    src_lang = test_dir.split("_")[2]
+	    if src_lang == 'en':
+	        src_lang = 'l1'
+	        src_vocab = l1_vocab
+	        trg_lang = 'l2'
+	        trg_vocab = l2_vocab
+	    elif src_lang == 'fr':
+	        src_lang = 'l2'
+	        src_vocab = l2_vocab
+	        trg_lang = 'l1'
+	        trg_vocab = l1_vocab
+	    else:
+	        ValueError('source language')
+	    
+	    test_dataset = MonolingualDataset(folder=test_dir, vocab=src_vocab)
+	    test_file = test_dataset._paths[0].split('/')[2]
+	    print test_file, src_lang
+	    f = open('test_translated/' + test_file + '_translated', 'w')
+	    for i in range(test_dataset._line_counts[0]):
+	        sample = test_dataset[i]
+	        # Hacks needed to get test batches of size 1
+	        src = Variable(torch.LongTensor(sample['src'])).unsqueeze(0)
+	        lengths = torch.LongTensor([sample['src_len']])
+	        # End hack
+	        src, lengths, _, _ = transform_inputs(
+	            src=src,
+	            lengths=lengths,
+	            tgt=src)
+	        translated = translate(model=model, 
+	            src=src, 
+	            src_lang=src_lang, 
+	            lengths=lengths, 
+	            beam_size=beam_size,
+	            max_trg_len=config.data.max_length, 
+	            target_vocab=trg_vocab)
+	        f.write(translated+'\n')
+	    f.close()
+	    print("Time to translate file (secs): ",  time.time() - start)
+
 
 
